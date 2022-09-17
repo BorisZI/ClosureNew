96c40c582f3902f3258ca762b9fc9c641870f077
Boris Z <Boris Z>
2022-09-17 17:39:36 +0300

    Возможности типов с висом функций у переменных и констант commit

--- ClosureNew/main.swift
+++ ClosureNew/main.swift
@@ -7,5 +7,37 @@
 
 import Foundation
 
-print("Hello, World!")
+
+func plus(a:Int, b:Int) -> Int {
+    a + b
+}
+
+func minis(a:Int, b:Int) -> Int {
+    a - b
+}
+
+
+var someFunc: (Int, Int) -> Int
+    
+    for num in 0..<2 {
+        if num == 0 {
+            someFunc = plus
+        } else {
+            someFunc = minis
+        }
+let result = someFunc(3, 5)
+        print(result)
+}
+
+func someNew(_ op1: Int, _ op2: Int, _ operation: (Int, Int) -> Int) -> Int {
+    return operation(op1, op2)
+}
+
+var resultN = someNew(3, 0) { $0 + $1 }
+print("New", resultN)
+
+//let result = someFunc(3, 5)
+ resultN = someNew(0, 5, +)
+print("New2", resultN)
+
 
